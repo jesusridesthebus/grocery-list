77e09e3d859dec0e6c3f990f890bde2e6168344e
Lindsey Baker and Liz Kelley <lindsey@>
2019-04-01 18:54:08 -0700

    add different logic in js to attempt to get the list to sort. previous commit didnt actually sort. does not work at the moment

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,37 @@
 $(function() {
   $("#grocery-form").submit(function(event) {
+    event.preventDefault();
     var groceries = ["item1", "item2", "item3", "item4", "item5"];
-    console.log("1", groceries);
+    var groceryList = [];
 
     groceries.forEach(function(grocery) {
-      var groceryInput = $("input#" + grocery).val();
-      $(".groceries").sort().append("<li>" + groceryInput.charAt(0).toUpperCase() + groceryInput.slice(1) + "</li>").val();
-
-      console.log("2", groceryInput);
+      var item = $("input#" + grocery).val();
+      groceryList.push(item);
+      console.log("groceries", groceries);
     });
-    event.preventDefault();
+
+    console.log("groceryList", groceryList);
+
+    groceryList.forEach(function(grocery) {
+      var newGrocery = $(".groceries-display").append("<li>" + groceryList.charAt(0).toUpperCase() + groceryList.slice(1) + "</li>").val();
+      newGrocery.sort();
+    });
+    console.log("newGrocery", newGrocery);
+
   });
 });
+
+// $(function() {
+//   $("#grocery-form").submit(function(event) {
+//     var groceries = ["item1", "item2", "item3", "item4", "item5"];
+//     console.log("1", groceries);
+//
+//     groceries.forEach(function(grocery) {
+//       var groceryInput = $("input#" + grocery).val();
+//       $(".groceries-display").append("<li>" + groceryInput.charAt(0).toUpperCase() + groceryInput.slice(1) + "</li>").val();
+//
+//       console.log("2", groceryInput);
+//     });
+//     event.preventDefault();
+//   });
+// });
