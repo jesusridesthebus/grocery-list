484f9a2abc9fd52b2006f7e712cdbc9306360e5e
Liz <jesusridesthebus@gmail.com>
2019-04-02 17:47:48 -0700

    remove commented out code

--- js/scripts.js
+++ js/scripts.js
@@ -17,24 +17,3 @@ $(function() {
     });
   });
 });
-
-// $(function() {
-//   $("#grocery-form").submit(function(event) {
-//     var groceries = ["item1", "item2", "item3", "item4", "item5"];
-//     var groceryList = [];
-
-//     groceries.forEach(function(grocery) {
-//       var groceryInput = $("input#" + grocery).val();
-//       groceryList.push(groceryInput);
-//       event.preventDefault();
-//     });
-
-//     groceryList = groceryList.sort();
-//     console.log("groceryList", groceryList);
-
-//     groceryList.forEach(function(grocery) {
-//       $(".groceries-display").append("<li>" + grocery.charAt(0).toUpperCase() + grocery.slice(1) + "</li>").val();
-//       event.preventDefault();
-//     });
-//   });
-// });
